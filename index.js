c307baa66391d7bc30c26e25f499c45285121a22
Cat Conti <conticatherine@gmail.com>
2017-08-30 13:49:16 -0700

    adding add folders

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function(req,res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(process.env.PORT || 3000, process.env.HOST || 'localhost', function() {
+var server = app.listen(process.env.PORT || 3000, function() {
 var host = server.address().address;
 var port = server.address().port;
 
