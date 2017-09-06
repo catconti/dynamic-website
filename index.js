6bf8f5ebb2b4a3785ffedb5a94df73b79c42fa49
Cat Conti <conticatherine@gmail.com>
2017-09-06 15:52:29 -0700

    corrected paths & index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.use('/public/css',express.static('css'));
-app.use('/public/img',express.static('img'));
-
 app.get('/', function(req,res) {
 	res.sendFile(__dirname + '/index.html');
 });
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/img', express.static(__dirname + '/img'));
 
 var server = app.listen(process.env.PORT || 3000, function() {
 var host = server.address().address;
